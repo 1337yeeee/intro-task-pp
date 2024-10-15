c7cdaaded7eface0b7b48bc3fd4237481ecc08aa
Anatoly Maksimov <amaksimov@perfectpanel.com>
2024-10-15 11:02:53 +0300

    initial commit

---
+++ application/config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2user',
+    'username' => 'yii2app',
+    'password' => 'yii2password',
+    'charset' => 'utf8',
+
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
