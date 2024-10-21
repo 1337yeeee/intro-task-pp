d76070c265f12baeabeb6531de5e44389e162678
Andtoly Maksimov <tolik.maks25@gmail.com>
2024-10-21 18:01:07 +0300

    added method getColumns

--- application/modules/orders/widgets/OrdersTable.php
+++ application/modules/orders/widgets/OrdersTable.php
@@ -26,65 +26,76 @@ class OrdersTable extends GridView
         return parent::widget([
             'dataProvider' => $dataProvider,
             'layout' => "{items}\n{$layoutPages}",
-            'columns' => [
-                [
-                    'attribute' => 'id',
-                    'label' => Yii::t('app', 'ID'),
-                ],
-                [
-                    'label' => Yii::t('app', 'User'),
-                    'value' => function ($model) {
-                        return $model->user->first_name . ' ' . $model->user->last_name;
-                    }
-                ],
-                [
-                    'attribute' => 'link',
-                    'label' => Yii::t('app', 'Link'),
-                ],
-                [
-                    'attribute' => 'quantity',
-                    'label' => Yii::t('app', 'Quantity'),
-                ],
-                [
-                    'attribute' => 'service_id',
-                    'header' => self::renderDropdownService($config['servicesList']),
-                    'headerOptions' => ['class' => 'dropdown-th'],
-                    'encodeLabel' => false,
-                    'value' => function ($model) {
-                        return '<span class="label-id">' . $model->service->id . '</span> ' . $model->service->name;
-                    },
-                    'format' => 'raw',
-                ],
-                [
-                    'attribute' => 'status',
-                    'label' => Yii::t('app', 'Status'),
-                    'filter' => [
-                        'Pending' => Yii::t('app', 'Pending'),
-                        'In progress' => Yii::t('app', 'In progress'),
-                        'Completed' => Yii::t('app', 'Completed'),
-                        'Canceled' => Yii::t('app', 'Canceled'),
-                        'Error' => Yii::t('app', 'Error'),
-                    ],
-                    'value' => function ($model) {
-                        return $model->getStatus();
-                    }
-                ],
-                [
-                    'attribute' => 'mode',
-                    'header' => self::renderDropdownMode(),
-                    'headerOptions' => ['class' => 'dropdown-th'],
-                    'encodeLabel' => false,
-                    'value' => function ($model) {
-                        return $model->getMode();
-                    }
-                ],
-                [
-                    'attribute' => 'created_at',
-                    'label' => Yii::t('app', 'Created'),
-                    'format' => ['date', 'php:Y-m-d H:i:s'],
-                ],
-            ],
+            'columns' => self::getColumns($config['servicesList']),
         ]);
+    }
+
+    /**
+     * Returns an array for 'columns' GridView parameter
+     *
+     * @param $servicesList
+     * @return array[]
+     */
+    private static function getColumns($servicesList): array
+    {
+        return [
+            [
+                'attribute' => 'id',
+                'label' => Yii::t('app', 'ID'),
+            ],
+            [
+                'label' => Yii::t('app', 'User'),
+                'value' => function ($model) {
+                    return $model->user->first_name . ' ' . $model->user->last_name;
+                }
+            ],
+            [
+                'attribute' => 'link',
+                'label' => Yii::t('app', 'Link'),
+            ],
+            [
+                'attribute' => 'quantity',
+                'label' => Yii::t('app', 'Quantity'),
+            ],
+            [
+                'attribute' => 'service_id',
+                'header' => self::renderDropdownService($servicesList),
+                'headerOptions' => ['class' => 'dropdown-th'],
+                'encodeLabel' => false,
+                'value' => function ($model) {
+                    return '<span class="label-id">' . $model->service->id . '</span> ' . $model->service->name;
+                },
+                'format' => 'raw',
+            ],
+            [
+                'attribute' => 'status',
+                'label' => Yii::t('app', 'Status'),
+                'filter' => [
+                    'Pending' => Yii::t('app', 'Pending'),
+                    'In progress' => Yii::t('app', 'In progress'),
+                    'Completed' => Yii::t('app', 'Completed'),
+                    'Canceled' => Yii::t('app', 'Canceled'),
+                    'Error' => Yii::t('app', 'Error'),
+                ],
+                'value' => function ($model) {
+                    return $model->getStatus();
+                }
+            ],
+            [
+                'attribute' => 'mode',
+                'header' => self::renderDropdownMode(),
+                'headerOptions' => ['class' => 'dropdown-th'],
+                'encodeLabel' => false,
+                'value' => function ($model) {
+                    return $model->getMode();
+                }
+            ],
+            [
+                'attribute' => 'created_at',
+                'label' => Yii::t('app', 'Created'),
+                'format' => ['date', 'php:Y-m-d H:i:s'],
+            ],
+        ];
     }
 
     /**
@@ -104,10 +115,8 @@ class OrdersTable extends GridView
 
         $currentServiceId = Yii::$app->request->get('service_id', '');
 
-        // Добавляем элемент "All"
         $dropdownHtml .= '<li class="' . ($currentServiceId === '' ? 'active' : '') . '"><a href="' . self::createUrlWithParam('service_id', '') . '">' . Yii::t('app', 'All') . ' (894931)</a></li>';
 
-        // Добавляем остальные сервисы
         foreach ($servicesList as $service) {
             $dropdownHtml .= '<li><a ' . ($currentServiceId === (string)$service['id'] ? 'active' : '') . ' href="' . self::createUrlWithParam('service_id', $service['id']) . '"><span class="label-id">' . $service['order_count'] . '</span> ' . $service['name'] . '</a></li>';
         }
