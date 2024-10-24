fc0e394ebd182b394a931180620efbd93b903258
Andtoly Maksimov <tolik.maks25@gmail.com>
2024-10-24 12:12:47 +0300

    cleared up main layout

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -5,18 +5,13 @@
 /** @var string $content */
 
 use app\assets\AppAsset;
-use app\widgets\Alert;
-use yii\bootstrap5\Breadcrumbs;
 use yii\bootstrap5\Html;
-use yii\bootstrap5\Nav;
-use yii\bootstrap5\NavBar;
 
 AppAsset::register($this);
 
-$this->registerCsrfMetaTags();
 $this->registerMetaTag(['charset' => Yii::$app->charset], 'charset');
-$this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, initial-scale=1']);
 $this->registerMetaTag(['name' => 'X-UA-Compatible', 'content' => 'IE=edge']);
+$this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, initial-scale=1']);
 $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii::getAlias('@web/favicon.ico')]);
 $this->registerCssFile('@web/css/bootstrap.min.css');
 $this->registerCssFile('@web/css/custom.css');
