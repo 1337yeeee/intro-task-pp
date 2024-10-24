4e535a3b42393e3331e2532ef4f2b9d0e772a923
Andtoly Maksimov <tolik.maks25@gmail.com>
2024-10-24 15:53:10 +0300

    url params from searchModel

--- application/modules/orders/widgets/views/ordersNavigation.php
+++ application/modules/orders/widgets/views/ordersNavigation.php
@@ -11,11 +11,15 @@ use yii\helpers\Url;
 ?>
 
 <ul class="nav nav-tabs p-b" id="nav-tabs">
+    <?php $search = $searchModel->search; ?>
+    <?php $search_type = $searchModel->search_type; ?>
+
     <?php foreach ($statuses as $status => $label): ?>
         <li class="<?= $searchModel->getStatus() === $status ? 'active' : '' ?>">
-            <a href="<?= Url::to(['', 'status' => $status, 'search' => $searchModel->search, 'search_type' => $searchModel->search_type]) ?>"><?= $label ?></a>
+            <a href="<?= Url::to(['', 'status' => $status, 'search' => $search, 'search_type' => $search_type]) ?>"><?= $label ?></a>
         </li>
     <?php endforeach; ?>
+
     <li class="pull-right custom-search">
         <?= OrdersSearchForm::widget([
             'searchModel' => $searchModel,
